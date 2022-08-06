8b9607fe139c59e6e91509cc6d09791c7c2a1b33
helenapaixao <hp.helenapaixao@gmail.com>
2022-08-06 02:14:40 -0300

    feat: high almost done

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,32 +1,38 @@
 import styled, { css, DefaultTheme } from 'styled-components'
+import media from 'styled-media-query'
 
 import { HeadingProps, LineColors } from '.'
 
 const wrapperModifiers = {
   small: (theme: DefaultTheme) => css`
     font-size: ${theme.font.sizes.medium};
-
     &::after {
       width: 3rem;
     }
   `,
+
   medium: (theme: DefaultTheme) => css`
     font-size: ${theme.font.sizes.xlarge};
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
   `,
+
   lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
     padding-left: ${theme.spacings.xxsmall};
     border-left: 0.7rem solid ${theme.colors[lineColor]};
   `,
-  lineBottom: (theme: DefaultTheme, LineColors: LineColors) => css`
+
+  lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
     position: relative;
     margin-bottom: ${theme.spacings.medium};
     &::after {
-      content: '';
       position: absolute;
       left: 0;
-      bottom: -1rem;
+      bottom: -0.5rem;
+      content: '';
       width: 5rem;
-      border-bottom: 0.5rem solid ${theme.colors[LineColors]};
+      border-bottom: 0.5rem solid ${theme.colors[lineColor]};
     }
   `
 }
@@ -34,8 +40,8 @@ const wrapperModifiers = {
 export const Wrapper = styled.h2<HeadingProps>`
   ${({ theme, color, lineLeft, lineBottom, lineColor, size }) => css`
     color: ${theme.colors[color!]};
-    ${!!size && wrapperModifiers[size](theme)};
-    ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor!)};
+    ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor!)}
     ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor!)}
+    ${!!size && wrapperModifiers[size](theme)}
   `}
 `
--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,10 +1,11 @@
+import Button from 'components/Button'
 import * as S from './styles'
-import Button from '../Button'
 
 export type HighlightProps = {
   title: string
   subtitle: string
   backgroundImage: string
+  floatImage?: string
   buttonLabel: string
   buttonLink: string
 }
@@ -12,11 +13,13 @@ export type HighlightProps = {
 const Highlight = ({
   title,
   subtitle,
+  backgroundImage,
+  floatImage,
   buttonLabel,
-  buttonLink,
-  backgroundImage
+  buttonLink
 }: HighlightProps) => (
   <S.Wrapper backgroundImage={backgroundImage}>
+    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
     <S.Content>
       <S.Title>{title}</S.Title>
       <S.Subtitle>{subtitle}</S.Subtitle>
