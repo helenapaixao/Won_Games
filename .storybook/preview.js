00ff1696315c77e571c87fa477b9d7d2f4a446e0
helenapaixao <hp.helenapaixao@gmail.com>
2022-08-07 16:09:53 -0300

    fix: config husky

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,6 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
-
 export const parameters = {
   nextRouter: {
     Provider: RouterContext.Provider
