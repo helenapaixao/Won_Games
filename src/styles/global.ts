9569800e588c7aab6b589f1526e5f8726e2586bc
helenapaixao <hp.helenapaixao@gmail.com>
2022-08-12 18:45:25 -0300

    feat: add bannerslider

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,26 @@
-import { createGlobalStyle, css } from 'styled-components'
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
-@font-face {
-  font-family:'Poppins';
-  font-style: normal;
-  font-weight: 300;
-  font-display: swap;
-  src: local('Poppins Light'), local('Poppins-Light'),
-        url('/fonts/Poppins-Light.woff2') format('woff2');
+type GlobalStylesProps = {
+  removeBg?: boolean
 }
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v12-latin-300.woff2') format('woff2');
+  }
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
@@ -31,25 +43,24 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
-
-${({ theme }) => css`
-  html {
-    font-size: 62.5%;
-  }
-  html,
-  body,
-  #__next {
-    height: 100%;
-  }
-
-  body {
-    font-family: ${theme.font.family};
-    font-size: ${theme.font.sizes.medium};
-  }
-`}
-
-
+  ${({ theme, removeBg }) => css`
+    html {
+      font-size: 62.5%;
+    }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+      ${!removeBg &&
+      css`
+        background-color: ${theme.colors.mainBg};
+      `}
+    }
+  `}
 `
 
 export default GlobalStyles
