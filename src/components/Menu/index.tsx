df50ef5f7d5214cc3e8f2d49258822fb7ff45684
helenapaixao <hp.helenapaixao@gmail.com>
2023-05-16 14:28:23 -0300

    chore: config menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,8 +3,10 @@ import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
+
 import Button from 'components/Button'
 import Logo from 'components/Logo'
+import MediaMatch from 'components/MediaMatch'
 import * as S from './styles'
 
 export type MenuProps = {
@@ -16,25 +18,43 @@ const Menu = ({ username }: MenuProps) => {
 
   return (
     <S.Wrapper>
-      <S.IconWrapper onClick={() => setIsOpen(true)}>
-        <MenuIcon arial-label="Open Menu" />
-      </S.IconWrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
+
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
-          <SearchIcon arial-label="Search" />
+          <SearchIcon aria-label="Search" />
         </S.IconWrapper>
         <S.IconWrapper>
-          <ShoppingCartIcon arial-label="Open Shopping Cart" />
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
+        {!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign in</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
+
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
           <S.MenuLink href="#">Home</S.MenuLink>
           <S.MenuLink href="#">Explore</S.MenuLink>
+
           {!!username && (
             <>
               <S.MenuLink href="#">My account</S.MenuLink>
@@ -42,6 +62,7 @@ const Menu = ({ username }: MenuProps) => {
             </>
           )}
         </S.MenuNav>
+
         {!username && (
           <S.RegisterBox>
             <Button fullWidth size="large">
@@ -49,7 +70,6 @@ const Menu = ({ username }: MenuProps) => {
             </Button>
             <span>or</span>
             <S.CreateAccount href="#" title="Sign Up">
-              {' '}
               Sign Up
             </S.CreateAccount>
           </S.RegisterBox>
