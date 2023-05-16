4230d8066774778b0ca716b3076b7181b6e02712
helenapaixao <hp.helenapaixao@gmail.com>
2023-05-16 13:54:34 -0300

    fix: config component menu

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from 'components/Container'
+
+import Menu from 'components/Menu'
+
+const Home = () => (
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+  </section>
+)
+
+export default Home
