7a8661b3b9b7c1f0662aeed4c56d24284a2c9112
helenapaixao <hp.helenapaixao@gmail.com>
2023-05-31 18:19:03 -0300

    feat: add styles

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,14 +3,21 @@ import * as S from './styles'
 export type CheckboxProps = {
   label?: string
   labelFor?: string
-  labelColor?: "white" | "black"
+  labelColor?: 'white' | 'black'
 }
 
-const Checkbox = ({label, labelFor ="", labelColor="white"} : CheckboxProps) => (
+const Checkbox = ({
+  label,
+  labelFor = '',
+  labelColor = 'white'
+}: CheckboxProps) => (
   <S.Wrapper>
-    <input id={labelFor} type="checkbox" />
-    {!!label && <S.Label htmlFor={labelFor} labelColor={labelColor}>{label}</S.Label>}
-
+    <S.Input id={labelFor} type="checkbox" />
+    {!!label && (
+      <S.Label htmlFor={labelFor} labelColor={labelColor}>
+        {label}
+      </S.Label>
+    )}
   </S.Wrapper>
 )
 
