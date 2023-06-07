d1d094c951f9c15f422af33fb76edfc20c97fb72
helenapaixao <hp.helenapaixao@gmail.com>
2023-06-07 18:24:04 -0300

    feat: add error in input

--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -18,10 +18,20 @@ export default {
     onInput: { action: 'changed' },
     icon: { type: '' }
   }
-} as Meta
+} as unknown as Meta
 
 export const Default: Story<TextFieldProps> = (args) => (
   <div style={{ maxWidth: 300, padding: 15 }}>
     <TextField {...args} />
   </div>
 )
+
+export const withError: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withError.args = {
+  error: 'Ops...something is wrong'
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -25,12 +25,13 @@ export default {
     primary: '#F231A5',
     secondary: '#3CD3C1',
     mainBg: '#06092B',
+    lightBg: '#F2F2F2',
     white: '#FAFAFA',
     black: '#030517',
     lightGray: '#EAEAEA',
     gray: '#8F8F8F',
     darkGray: '#2E2F42',
-    lightBg: '#F2F2F2'
+    red: '#FF6347'
   },
   spacings: {
     xxsmall: '0.8rem',
