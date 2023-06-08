f3322dd65cd242c71e3c0e2624ac7e62b3edb4bf
helenapaixao <hp.helenapaixao@gmail.com>
2023-06-08 20:39:34 -0300

    feat: create show case

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,12 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HighlightProps } from '.'
 import item from './mock'
+
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    ...item
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<HighlightProps> = (args) => (
