656d61668d70f6f3deffe2437733bed092e37134
helenapaixao <hp.helenapaixao@gmail.com>
2023-06-26 12:16:58 -0300

    feat:create structure with styles and story

---
+++ src/components/TextContent/index.tsx
@@ -0,0 +1,19 @@
+import Heading from 'components/Heading'
+
+import * as S from './styles'
+
+export type  TextContentProps = {
+  title?: string
+  content: string
+}
+
+const TextContent = ({title, content}: TextContentProps) => <S.Wrapper>
+  <Heading lineLeft lineColor='secondary'>
+    {!!title && (
+      <Heading lineLeft lineColor='secondary'>
+        {title}
+        </Heading>
+    )}
+    <div dangerouslySetInnerHTML={{__html: content}}/>
+  </Heading>
+</S.Wrapper>
