2e4fa91a62eec6b2f62db2859bb44ac4e910d257
helenapaixao <hp.helenapaixao@gmail.com>
2023-06-26 10:50:15 -0300

    feat: add rating and genres

---
+++ src/components/GameDetails/mock.ts
@@ -0,0 +1,8 @@
+export default {
+developer: "Different Tales",
+releaseData: "2023-06-21T23:00:00",
+platforms:['windows','mac', 'linux'],
+publisher: 'Walkabout',
+rating: 'BR0',
+genres: ['Role-playing']
+}
