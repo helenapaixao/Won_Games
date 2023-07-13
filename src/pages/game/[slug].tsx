04ce447fe928669eb7ec588fee1cb17fce5f52a8
helenapaixao <hp.helenapaixao@gmail.com>
2023-07-13 13:38:35 -0300

    feat: add gamedetails in page

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -41,7 +41,16 @@ export async function getStaticProps() {
           'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality'
       },
       gallery: GalleryMock,
-      description: descriptionHTML
+      description: descriptionHTML,
+      details: {
+          developer: 'CD PROJEKT',
+          releaseDate: '2023-11-21T23:00:00',
+          platforms: ['windows'],
+          publisher: 'CD PROJEKT',
+          rating: 'BR0',
+          genres: ['Role-playing']
+
+      }
     }
   }
 }
--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -4,15 +4,17 @@ import * as S from './styles'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
 import TextContent from 'components/TextContent'
+import GameDetails, { GameDetailsProps } from 'components/GameDetails'
 
 export type GameTemplateProps = {
   cover: string
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
   description: string
+  details: GameDetailsProps
 }
 
-const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery, description, details }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
     <S.Main>
@@ -25,6 +27,9 @@ const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
       <S.SectionDescription>
         <TextContent title="Description" content={description} />
       </S.SectionDescription>
+      <S.SectionGameDetails>
+        <GameDetails {...details}/>
+      </S.SectionGameDetails>
     </S.Main>
   </Base>
 )
