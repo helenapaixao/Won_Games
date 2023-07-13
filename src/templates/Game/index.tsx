642912bbf47df83dd1e416ee1d7b3d7a283d850d
helenapaixao <hp.helenapaixao@gmail.com>
2023-07-13 13:20:28 -0300

    feat: add TextContent in game description

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -3,14 +3,16 @@ import Base from 'templates/Base'
 import * as S from './styles'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
+import TextContent from 'components/TextContent'
 
 export type GameTemplateProps = {
   cover: string
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
+  description: string
 }
 
-const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
     <S.Main>
@@ -20,6 +22,9 @@ const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
       <S.SectionGallery>
         {!!gallery && <Gallery items={gallery} />}
       </S.SectionGallery>
+      <S.SectionDescription>
+        <TextContent title="Description" content={description} />
+      </S.SectionDescription>
     </S.Main>
   </Base>
 )
