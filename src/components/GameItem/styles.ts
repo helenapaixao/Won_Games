fac9e8538318bb6628e3c045fcd482b26a971d5a
helenapaixao <hp.helenapaixao@gmail.com>
2023-07-15 15:21:53 -0300

    feat: create Cartlist

--- src/components/GameItem/styles.ts
+++ src/components/GameItem/styles.ts
@@ -1,13 +1,13 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
-export const Wrapper = styled.main`
+
+export const Wrapper = styled.div`
   ${({ theme }) => css`
     padding: ${theme.spacings.small};
     border-bottom: 0.1rem solid ${theme.colors.lightGray};
 
     ${media.greaterThan('medium')`
-    display:flex;
-
+      display: flex;
     `}
   `}
 `
@@ -28,11 +28,12 @@ export const ImageBox = styled.div`
       height: 100%;
       object-fit: cover;
     }
+
     ${media.greaterThan('medium')`
-margin-right: ${theme.spacings.xsmall};
-width:15rem;
-height:7rem;
-`}
+      margin-right: ${theme.spacings.xsmall};
+      width: 15rem;
+      height: 7rem;
+    `};
   `}
 `
 
@@ -47,16 +48,17 @@ export const Title = styled.h3`
     font-size: ${theme.font.sizes.small};
     line-height: ${theme.font.sizes.small};
     font-weight: ${theme.font.bold};
+    color: ${theme.colors.black};
     margin-bottom: ${theme.spacings.xxsmall};
-    ${media.greaterThan('medium')`]
 
-    font-size: 2rem;
-    line-height: 2rem
-    `}
+    ${media.greaterThan('medium')`
+      font-size: 2rem;
+      line-height: 2rem;
+    `};
   `}
 `
 
-export const Price = styled.p`
+export const Price = styled.div`
   ${({ theme }) => css`
     color: ${theme.colors.white};
     padding: 0.2rem ${theme.spacings.xxsmall};
@@ -64,5 +66,46 @@ export const Price = styled.p`
     border-radius: ${theme.border.radius};
     font-size: ${theme.font.sizes.small};
     font-weight: ${theme.font.bold};
+  `}
+`
+
+export const DownloadLink = styled.a`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    margin-left: ${theme.spacings.xxsmall};
+  `}
+`
+
+export const PaymentContent = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray};
+    font-size: ${theme.font.sizes.small};
+    display: flex;
+    flex-direction: column;
+    min-width: 28rem;
+    margin-top: ${theme.spacings.xsmall};
+
+    ${media.greaterThan('medium')`
+      margin-top: 0;
+      flex: 1;
+      flex-direction: column-reverse;
+      justify-content: space-between;
+      align-items: flex-end;
+    `}
+  `}
+`
+
+export const CardInfo = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-left: ${theme.spacings.xxsmall};
+    }
+
+    ${media.lessThan('medium')`
+      margin-top: ${theme.spacings.xsmall};
+    `}
   `}
 `
