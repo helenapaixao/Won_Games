8e325fd7ec8bdd4b161b74c3613a6ab415ce6281
helenapaixao <hp.helenapaixao@gmail.com>
2023-07-15 16:00:34 -0300

    feat: create struture paymentOptions

--- src/components/PaymentOptions/index.tsx
+++ src/components/PaymentOptions/index.tsx
@@ -1,7 +1,42 @@
+import Heading from 'components/Heading'
 import * as S from './styles'
+import Radio from 'components/Radio'
 
-const PaymentOptions = () => (
+export type PaymentOptionsProps = {
+  cards?: PaymentCard[]
+  handlePayment: () => void
+}
+
+export type PaymentCard = {
+  number: string
+  flag: string
+  img: string
+}
+
+const PaymentOptions = ({ cards, handlePayment }: PaymentOptionsProps) => (
   <S.Wrapper>
+    <S.Body>
+      <Heading color="black" lineBottom>
+        Payment
+      </Heading>
+      <S.CardList>
+        {cards?.map((card) => (
+          <S.CardItem key={card.number}>
+            <S.CardInfo>
+              <img src={card.img} alt={card.flag} />
+              {card.number}
+            </S.CardInfo>
+            <Radio
+              onCheck={() => {}}
+              name="credit-card"
+              id={card.number}
+              value={card.number}
+            />
+          </S.CardItem>
+        ))}
+      </S.CardList>
+    </S.Body>
+    <S.Footer></S.Footer>
   </S.Wrapper>
 )
 
--- src/components/PaymentOptions/styles.ts
+++ src/components/PaymentOptions/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main``
+
+
+export const Body = styled.div` `
+
+
+export const Footer = styled.div``
+
+
+export const CardList = styled.div``
+
+
+export const CardItem = styled.div``
+
+
+export const CardInfo = styled.div``
